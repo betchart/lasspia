55a834afa52499956f8ce71312327fd7ddb7f16d
Burton Betchart <betchart@gmail.com>
2017-11-08 02:35:56 -0500

    add Rang to preprocessing

--- routines/preprocessing.py
+++ routines/preprocessing.py
@@ -10,19 +10,47 @@ class preprocessing(baofast.routine):
     def __call__(self):
         ctlg = self.config.catalogRandom()
         self.hdus.append( self.pdfZ(ctlg) )
+        self.hdus.append( self.Rang(ctlg) )
         self.writeToFile()
 
+    @staticmethod
+    def addProvenance(hdu, inputFiles):
+        for i,f in enumerate(inputFiles):
+            hdu.header['prov%d'%i] = (f.split('/')[-1],
+                                      "Source data file.")
+
     def pdfZ(self, ctlg):
-        frq, edges = np.histogram(ctlg.z, self.config.binsZ(), # use scipy.stats.binned_statistic if want to keep mean value
+        frq, edges = np.histogram(ctlg.z, self.config.binsZ(),
                                   weights = ctlg.weight / sum(ctlg.weight))
+
         pdfz = np.array(zip(edges, frq),
                         dtype = [("lowEdge", np.float64),
                                  ("probability", np.float32)])
-        hdu = fits.BinTableHDU(pdfz)
-        hdu.header['name'] = ("pdfZ",
-                              "Redshift probability density.")
-        for i,f in enumerate(self.config.inputFilesRandom()):
-            hdu.header['prov%d'%i] = (f.split('/')[-1],
-                                      "File from which pdfZ was constructed.")
+
+        hdu = fits.BinTableHDU(pdfz, name="pdfZ")
+        #hdu.header['name'] = ("pdfZ", "Redshift probability density.")
+        self.addProvenance(hdu, self.config.inputFilesRandom())
+
         return hdu
 
+    def Rang(self, ctlg):
+
+        frq, xedges, yedges = np.histogram2d(ctlg.ra, ctlg.dec,
+                                             [self.config.binsRA(),
+                                              self.config.binsDec()])
+        xx, yy = np.meshgrid(xedges, yedges)
+
+        rang = np.array(zip(xx.flat, yy.flat, frq.flat),
+                        dtype = [("lowEdgeRA", np.float64),
+                                 ("lowEdgeDec", np.float64),
+                                 ("count", np.float32)])
+
+        hdu = fits.BinTableHDU(rang[frq.flat>0], name="Rang")
+        #hdu.header['name'] = ("Rang", "2D angular distribution, random catalog.")
+        self.addProvenance(hdu, self.config.inputFilesRandom())
+
+        return hdu
+
+# use scipy.stats.binned_statistic if want to keep mean value
+
+# consider storing bin centers rather than edges
