a72cae401e0b62a3e837d71f878cba28743c77ae
Burton Betchart <betchart@gmail.com>
2017-11-16 23:59:32 -0500

    simplify ThetaChunker; factor out 'chunks' function

--- routines/combinatorial.py
+++ routines/combinatorial.py
@@ -3,30 +3,25 @@ import math
 import numpy as np
 from scipy.sparse import csr_matrix
 from astropy.io import fits
-import sys
 
 DEGTORAD = math.pi / 180
 
 class ThetaChunker(object):
 
-    @staticmethod
-    def trigProducts(centersDec, centersRA):
+    def __init__(self, centersDec, centersRA, binsDec, binsRA):
         sinDec = np.sin(centersDec)
         cosDec = np.cos(centersDec)
-        return {'sinsinDec': np.multiply.outer(sinDec,sinDec),
-                'coscosDec': np.multiply.outer(cosDec,cosDec),
-                'cosDeltaRA': np.cos(np.subtract.outer(centersRA,centersRA))}
-
-    def __init__(self, trigPrd, binsDecRA1, binsDecRA2):
-        for k,v in trigPrd.iteritems(): setattr(self, k, v)
-        self.binsDec1, self.binsRA1 = binsDecRA1
-        self.binsDec2, self.binsRA2 = binsDecRA2
+        self.sinsinDec = np.multiply.outer(sinDec,sinDec)
+        self.coscosDec = np.multiply.outer(cosDec,cosDec)
+        self.cosDeltaRA = np.cos(np.subtract.outer(centersRA,centersRA))
+        self.binsDec = binsDec
+        self.binsRA = binsRA
 
     def __call__(self, slice1, slice2):
         cosTheta = (
-            self.cosDeltaRA[self.binsRA1[slice1]][ :,self.binsRA2[slice2]] *
-            self.coscosDec[self.binsDec1[slice1]][:,self.binsDec2[slice2]] +
-            self.sinsinDec[self.binsDec1[slice1]][:,self.binsDec2[slice2]])
+            self.cosDeltaRA[self.binsRA[slice1]][ :,self.binsRA[slice2]] *
+            self.coscosDec[self.binsDec[slice1]][:,self.binsDec[slice2]] +
+            self.sinsinDec[self.binsDec[slice1]][:,self.binsDec[slice2]])
         np.clip(cosTheta,-1,1,out=cosTheta)
         return np.arccos(cosTheta,out=cosTheta)
 
@@ -34,10 +29,6 @@ class ThetaChunker(object):
 class combinatorial(baofast.routine):
 
     def __call__(self):
-        self.trig = ThetaChunker.trigProducts(
-            DEGTORAD * self.getPre("centerDec").data["binCenter"],
-            DEGTORAD * self.getPre("centerRA").data["binCenter"] )
-
         self.hdus.append( self.binCentersTheta() )
         self.hdus.extend( self.fguOfTheta() )
         self.writeToFile()
@@ -47,21 +38,28 @@ class combinatorial(baofast.routine):
                             dtype = [("binCenter", np.float64)])
         return fits.BinTableHDU(centers, name="centerTheta")
 
+    def chunks(self, size):
+        # Full array of indices possible in place of slices: regioning.
+        # However, fancy indices makes a copy, while slice makes a view
+        splits = range(0, size, self.config.chunkSize())
+        slices = [slice(i,j) for i,j in zip(splits,splits[1:]+[None])]
+        return [(slices[i],jSlice)
+                for i in range(len(slices))
+                for jSlice in slices[i:]][self.iJob::self.nJobs]
+
     def fguOfTheta(self):
         ang = self.getPre("ANG").data
         angzd = csr_matrix(self.getPre("ANGZD").data)
-        cType = np.int64 if np.issubdtype(type(ang["countR"][0]), np.integer) else np.float64
+        cType = np.int64 if np.issubdtype(ang["countR"].dtype, np.integer) else np.float64
         frq = np.zeros(len(self.config.edgesTheta())-1, dtype=cType)
-        binsDecRA = (ang['binDec'], ang['binRA'])
-        thetaChunk = ThetaChunker(self.trig, binsDecRA, binsDecRA)
 
-        splits = range(0, len(ang), self.config.chunkSize())
-        slices = [slice(i,j) for i,j in zip(splits,splits[1:]+[None])] # full array of indices possible in place of slices: regioning
-        chunks = [(slices[i],jSlice)
-                  for i in range(len(slices))
-                  for jSlice in slices[i:]][self.iJob::self.nJobs]
+        thetaChunk = ThetaChunker(
+            DEGTORAD * self.getPre("centerDec").data["binCenter"],
+            DEGTORAD * self.getPre("centerRA").data["binCenter"],
+            ang['binDec'], ang['binRA'])
 
-        for slice1,slice2 in chunks:
+        for slice1,slice2 in self.chunks(len(ang)):
+            equalSlices = slice1 == slice2
             print '.'
             chunkT = thetaChunk(slice1, slice2)
             countR1 = ang["countR"][slice1]
@@ -69,14 +67,14 @@ class combinatorial(baofast.routine):
             zD1 = angzd[slice2]
             zD2 = angzd[slice2]
 
-            ccR = np.multiply.outer(countR1, countR2).astype(cType)
-            if slice1 != slice2: ccR *= 2 # fill histogram with twice-weights
+            ccR = np.multiply.outer(countR1, countR2).astype(frq.dtype)
+            if not equalSlices: ccR *= 2 # fill histogram with twice-weights
             frq += np.histogram( chunkT, weights = ccR,
                                  **self.config.binningTheta())[0]
 
             iAng, iZ = zD2.nonzero()
             thetas = chunkT[:,iAng]
-            iZs = np.multiply.outer(np.ones(len(countR1), dtype=np.int16), iZ).astype(np.int16)
+            iZs = np.multiply.outer(np.ones(len(countR1), dtype=np.int16), iZ.astype(np.int16))
             weight = np.multiply.outer(countR1, zD2.data) # relies on zD2.data in order with zD2.nonzero()
 
             zBins = zD1.shape[1]
@@ -90,7 +88,7 @@ class combinatorial(baofast.routine):
 
         if self.iJob is None:
             frq /= 2
-        fTheta = np.array(frq, dtype = [('count',cType)])
+        fTheta = np.array(frq, dtype = [('count',frq.dtype)])
         hdu = fits.BinTableHDU(fTheta, name="fTheta")
         return [hdu]
 
