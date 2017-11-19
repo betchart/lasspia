4a8024b58cc9480619f9caa4be5039000d1e92fe
Burton Betchart <betchart@gmail.com>
2017-11-19 13:30:05 -0500

    start building the integration step.

---
+++ routines/integration.py
@@ -0,0 +1,16 @@
+import baofast
+from astropy.io import fits
+
+class integration(baofast.routine):
+
+    def __call__(self):
+        print "Hello integration step."
+
+    def RR(self):
+        pass
+
+    def DR(self):
+        pass
+
+    def DD(self):
+        pass
