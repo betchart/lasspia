dab5457850dfe683ac04efeeda85292a58930657
Burton Betchart <betchart@gmail.com>
2017-11-27 01:24:17 -0500

    factor into functions

--- routines/integration.py
+++ routines/integration.py
@@ -1,60 +1,63 @@
-import baofast
 import math
-from astropy.io import fits
 import numpy as np
+import baofast as bf
+from astropy.io import fits
 from scipy.integrate import quad
 
-class integration(baofast.routine):
+class integration(bf.routine):
 
     def __call__(self):
+        self.pdfz = self.getInput('pdfZ').data['probability']
+        s = np.sqrt(sum(np.power(a,2) for a in self.sigmaPiGrids()))
+        centers = bf.utils.centers(self.config.edgesFromBinning(self.config.binningS()))
+        RR = self.calcRR(s)
+        DR = self.calcDR(s)
+        DD = self.calcDD(s)
+        for x,y,z,a in zip(centers,RR,DR,DD): print x,y,z,a
+        return
+
+    def sigmaPiGrids(self):
+        '''A cubic grid of sigma (pi) values
+        for pairs of galaxies with coordinates (iTheta, iZ1, iZ2).'''
         Iz = self.zIntegral()
         rOfZ = Iz * (self.config.lightspeed()/self.config.H0())
         tOfZ = rOfZ * (1 + self.config.omegasMKL()[1]/6 * Iz**2)
-        zCenters = self.getInput('centerZ').data['binCenter']
+
         thetas = self.getInput('centertheta').data['binCenter']
         sinT2 = np.sin(thetas/2)
         cosT2 = np.cos(thetas/2)
 
-        ft = self.getInput('fTheta').data['count']
-        gtz = self.getInput('gThetaZ').data
-        utzz = self.getInput('uThetaZZ').data
-        pdfz = self.getInput('pdfZ').data['probability']
-
         sigmas = sinT2[:,None,None] * (tOfZ[None,:,None] + tOfZ[None,None,:])
         pis = cosT2[:,None,None] * (rOfZ[None,:,None] - rOfZ[None,None,:])
-        s = np.sqrt(sigmas**2 + pis**2)
+        return sigmas, pis
 
-        def calcRR():
-            counts = ft[:,None,None] * pdfz[None,:,None] * pdfz[None,None,:]
-            rr = np.histogram(s, weights=counts, **self.config.binningS())[0]
-            rr /= np.sum(rr)
-            del counts
-            return rr
+    def calcRR(self,s):
+        ft = self.getInput('fTheta').data['count']
+        counts = ft[:,None,None] * self.pdfz[None,:,None] * self.pdfz[None,None,:]
+        rr = np.histogram(s, weights=counts, **self.config.binningS())[0]
+        rr /= np.sum(rr)
+        del counts
+        return rr
 
-        def calcDR():
-            counts = gtz[:,:,None] * pdfz[None,None,:]
-            dr = np.histogram(s, weights=counts, **self.config.binningS())[0]
-            dr /= np.sum(dr)
-            del counts
-            return dr
+    def calcDR(self,s):
+        gtz = self.getInput('gThetaZ').data
+        counts = gtz[:,:,None] * self.pdfz[None,None,:]
+        dr = np.histogram(s, weights=counts, **self.config.binningS())[0]
+        dr /= np.sum(dr)
+        del counts
+        return dr
 
-        def calcDD():
-            counts = utzz['count']
-            iThetas = utzz['binTheta']
-            iZdZ = utzz['binZdZ']
-            iZ = iZdZ / len(zCenters)
-            diZ = iZdZ % len(zCenters)
-            iZ2 = iZ + diZ
-            dd = np.histogram(s[iThetas,iZ,iZ2], weights=counts, **self.config.binningS())[0]
-            dd /= np.sum(dd)
-            return dd
-
-        centers = baofast.utils.centers(self.config.edgesFromBinning(self.config.binningS()))
-        RR = calcRR()
-        DR = calcDR()
-        DD = calcDD()
-        for x,y,z,a in zip(centers,RR,DR,DD): print x,y,z,a
-        return
+    def calcDD(self,s):
+        utzz = self.getInput('uThetaZZ').data
+        counts = utzz['count']
+        iThetas = utzz['binTheta']
+        iZdZ = utzz['binZdZ']
+        iZ = iZdZ / s.shape[1]
+        diZ = iZdZ % s.shape[1]
+        iZ2 = iZ + diZ
+        dd = np.histogram(s[iThetas,iZ,iZ2], weights=counts, **self.config.binningS())[0]
+        dd /= np.sum(dd)
+        return dd
 
     def zIntegral(self):
         zCenters = self.getInput('centerz').data['binCenter']
