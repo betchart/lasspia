d3e8922b9aa84639601356c83308e204aef35f41
Burton Betchart <betchart@gmail.com>
2017-12-05 01:42:58 -0500

    coarse configs inherit standard configs and redefine binning

--- configs/cmassN_coarse.py
+++ configs/cmassN_coarse.py
@@ -1,44 +1,11 @@
 import lasspia as La
 import math
+from cmassN import cmassN
 
-class cmassN_coarse(La.configuration):
-
-    def dataDir(self):
-        """Directory of catalog files."""
-        return '../data/'
-
-    def outputLocation(self):
-        return self.dataDir()
-
-    def inputFilesRandom(self):
-        return [self.dataDir() + "randoms_DR9_CMASS_North.fits"]
-
-    def inputFilesObserved(self):
-        return [self.dataDir() + "galaxies_DR9_CMASS_North.fits"]
-
-    def catalogRandom(self): return La.wrapRandomSDSS(self.inputFilesRandom())
-    def catalogObserved(self): return La.wrapObservedSDSS(self.inputFilesObserved())
+class cmassN_coarse(cmassN):
 
     def binningZ(self): return {"bins":300, "range":(0.43,0.7)}
     def binningRA(self): return {"bins":1200 , "range":(105,265)}
     def binningDec(self): return {"bins":600, "range":(-4,57)}
     def binningTheta(self): return {"bins":1000, "range":(0,math.pi)}
 
-    def chunkSize(self) : return 2000
-
-
-    '''Configuration affecting only the "integration" routine.'''
-
-    def omegasMKL(self):
-        '''Cosmology parameters (\Omega_M, \Omega_K, \Omega_\Lambda).'''
-        return (0.274, 0, 0.726)
-
-    def H0(self):
-        '''Hubble constant in (h km/s / Mpc)'''
-        return 100.
-
-    def lightspeed(self):
-        '''Speed of light in km/s'''
-        return 299792
-
-    def binningS(self): return {"bins":1200, "range":(0,6000)}
--- configs/cmassS_coarse.py
+++ configs/cmassS_coarse.py
@@ -1,47 +1,10 @@
 import lasspia as La
 import math
+from cmassS import cmassS
 
-class cmassS_coarse(La.configuration):
-
-    def dataDir(self):
-        """Directory of catalog files."""
-        return '../data/'
-
-    def outputLocation(self):
-        return self.dataDir()
-
-    def inputFilesRandom(self):
-        return [self.dataDir() + "randoms_DR9_CMASS_South.fits"]
-
-    def inputFilesObserved(self):
-        return [self.dataDir() + "galaxies_DR9_CMASS_South.fits"]
-
-    def catalogRandom(self):
-        return La.wrapRandomSDSS(self.inputFilesRandom(), shiftRA=True)
-
-    def catalogObserved(self):
-        return La.wrapObservedSDSS(self.inputFilesObserved(), shiftRA=True)
+class cmassS_coarse(cmassS):
 
     def binningZ(self): return {"bins":300, "range":(0.43,0.7)}
     def binningRA(self): return {"bins": 750, "range":(-50,50)}
     def binningDec(self): return {"bins":275, "range":(-10,20)}
     def binningTheta(self): return {"bins":1000, "range":(0,math.pi)}
-
-    def chunkSize(self): return 2000
-
-
-    '''Configuration affecting only the "integration" routine.'''
-
-    def omegasMKL(self):
-        '''Cosmology parameters (\Omega_M, \Omega_K, \Omega_\Lambda).'''
-        return (0.274, 0, 0.726)
-
-    def H0(self):
-        '''Hubble constant in (h km/s / Mpc)'''
-        return 100.
-
-    def lightspeed(self):
-        '''Speed of light in km/s'''
-        return 299792
-
-    def binningS(self): return {"bins":1200, "range":(0,6000)}
