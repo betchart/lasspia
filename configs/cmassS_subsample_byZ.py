2add7188e462d38650a9394974bfb3bb3d8f0096
Burton Betchart <betchart@gmail.com>
2018-01-28 21:50:24 -0500

    inherit from subsample rather that z-scliced cmassS

--- configs/cmassS_subsample_byZ.py
+++ configs/cmassS_subsample_byZ.py
@@ -1,5 +1,8 @@
-from cmassS_byZ import cmassS_byZ
+from cmassS_subsample import cmassS_subsample
+from lasspia.zSlicing import zSlicing
 
-class cmassS_subsample_byZ(cmassS_byZ):
-    def binningRA(self): return {"bins": 220, "range":(0,10)}
-    def binningDec(self): return {"bins":156, "range":(0,6)}
+class cmassS_subsample_byZ(zSlicing(cmassS_subsample)):
+
+    # zSlicing
+    def zBreaks(self): return [0.43, 0.58, 0.7]
+    def zMaxBinWidths(self): return [0.0003, 0.0003]
