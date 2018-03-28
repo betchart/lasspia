2f7f22abd1cad4c7f70afb299258a8f660bb88a6
Burton Betchart <betchart@gmail.com>
2018-03-28 15:46:34 -0400

    read variationsOmegasMKL from configuration

--- routines/multiIntegration.py
+++ routines/multiIntegration.py
@@ -5,20 +5,34 @@ class multiIntegration(integration):
     '''Perform integration multiple times, scanning the integration parameters.
 
     This class is meant to be an example of how one might achieve such
-    a scan.  The values to scan (here variationsOmegaMKL) would go
-    better in the configuration file.  The pattern we use is to
-    inherit from the 'integration' routine, and repeatedly call its
-    methods after changing parameters.  Notice that the output file
-    name also changes with parameters.
+    a scan.  The pattern used is to inherit from the 'integration'
+    routine, and repeatedly call its methods after changing
+    parameters.  Notice that the output file name also changes with
+    parameters.
 
     Similar results could be achieved by creating a new instance of
     the 'integration' routine for every iteration of the parameters.
     One might also choose to inherit from the 'integration' routine,
     but micromanage the hdu list by calling more specific methods,
     like integration.tpcf.
+
     '''
 
-    variationsOmegasMKL = [(0.273, 0, 0.726), (0.274, 0, 0.726), (0.275, 0, 0.726)]
+    @property
+    def variationsOmegasMKL(self):
+        try:
+            return self.config.variationsOmegasMKL()
+        except:
+            default = [(0.273, 0, 0.726), (0.274, 0, 0.726), (0.275, 0, 0.726)]
+            if not (hasattr(self, 'warned') and self.warned):
+                print('Warning: variationsOmegasMKL() is not defined in the configuration!')
+                print('multiIntegration will proceed using the default variations:')
+                print(default)
+                self.warned = True
+            return default
+
+    @property
+    def nVariations(self): return len(self.variationsOmegasMKL)
 
     def omegasMKL(self): return self.variationsOmegasMKL[self.iOmegas]
     def H0(self): return self.config.H0()
@@ -31,18 +45,23 @@ class multiIntegration(integration):
         return pre + index + '.fits' + suf
 
     def __call__(self):
-        for i in range(len(self.variationsOmegasMKL)):
+        for i in range(self.nVariations):
             self.iOmegas = i
             super(multiIntegration, self).__call__()
 
         return
 
     def combineOutput(self):
-        for i in range(len(self.variationsOmegasMKL)):
+        for i in range(self.nVariations):
             self.iOmegas = i
             super(multiIntegration, self).combineOutput()
 
     def plot(self):
-        for i in range(len(self.variationsOmegasMKL)):
+        for i in range(self.nVariations):
             self.iOmegas = i
             super(multiIntegration, self).plot()
+
+    def showFitsHeaders(self):
+        for i in range(self.nVariations):
+            self.iOmegas = i
+            super(multiIntegration, self).showFitsHeaders()
